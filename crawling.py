0f85011e779d050fce689d60ab322a3844726733
dandihara <gkaqhd0000@naver.com>
2022-04-09 23:14:14 +0900

    set naver api & test

---
+++ crawling.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import urllib.request
+
+#client 정보
+import my_key
+client_id = my_key.client_id
+client_secret = my_key.client_secret
+encText = urllib.parse.quote("수퍼트리")
+url = "https://openapi.naver.com/v1/search/news?query=" + encText # json 결과
+# url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
+request = urllib.request.Request(url)
+request.add_header("X-Naver-Client-Id",client_id)
+request.add_header("X-Naver-Client-Secret",client_secret)
+response = urllib.request.urlopen(request)
+rescode = response.getcode()
+if(rescode==200):
+    response_body = response.read()
+    print(response_body.decode('utf-8'))
+else:
+    print("Error Code:" + rescode)
